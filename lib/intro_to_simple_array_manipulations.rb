f62cf4d20a8d5f1b4daa62a9456c85b143fe504b
Valentin Placido <valentinplacido25@gmail.com>
2020-01-15 21:35:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,4 +23,25 @@ def shift_with_args(array)
 end
 
 def using_concat(arr1, arr2)
-  
+  arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  array.insert(element, 4)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string) 
+  array.delete(string)
+end
+
+def using_delete_at(array, integer) 
+  array.delete(integer)
+end
